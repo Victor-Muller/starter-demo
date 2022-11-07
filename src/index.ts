e4a641151dc40eb24e1ebce09e10d649b2360638
Victor-Muller <victor.muller@abelia.agency>
2022-11-08 00:47:42 +0100

    Update Gsap install

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-import { greetUser } from '$utils/greet';
+import { gsap } from 'gsap';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const animationTitles = gsap.to('.hero-item-title_wrapper', {
+    scale: 1.5,
+    duration: 1.6,
+    ease: 'power4.out',
+    paused: true,
+  });
+
+  const animationCard = gsap.to('.hero-list_item.first', {
+    flexGrow: 4,
+    duration: 1.6,
+    ease: 'power4.out',
+    paused: true,
+  });
+
+  const cardItem = document.querySelector('.hero-item-content_wrapper');
+
+  cardItem.addEventListener('mouseover', () => {
+    animationTitles.play();
+    animationCard.play();
+  });
+
+  cardItem.addEventListener('mouseout', () => {
+    animationTitles.reverse();
+    animationCard.reverse();
+  });
 });
