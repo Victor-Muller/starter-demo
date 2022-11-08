ba99b2469dbf9e6c38b9ab12719ef8b48bca107f
Victor-Muller <victor.muller@abelia.agency>
2022-11-08 13:03:29 +0100

    Animation hero fonctionnelle

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,30 @@ import { gsap } from 'gsap';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const animationTitles = gsap.to('.hero-item-title_wrapper', {
-    scale: 1.5,
-    duration: 1.6,
-    ease: 'power4.out',
-    paused: true,
+  $('[hero-card]').each(function () {
+    let bigTitle = $(this).find('.hero-item-title_wrapper');
+    let detailsTitle = $(this).find('.hero-details_h2');
+    let detailsDivider = $(this).find('.hero-details_divider');
+    let detailsText = $(this).find('.hero-details_text');
+    let card = $(this);
+    let tl = gsap.timeline();
+
+    tl.to(card, { flexGrow: 4 }, 0)
+      .to(bigTitle, { scale: 1.4, ease: 'circ.out ' }, 0)
+      .from(detailsTitle, { opacity: 0 }, 0)
+      .from(detailsDivider, { width: 0 }, 0)
+      .from(detailsText, { opacity: 0, y: 20 }, 0)
+      .reversed(true);
+    this.animation = tl;
   });
 
-  const animationCard = gsap.to('.hero-list_item.first', {
-    flexGrow: 4,
-    duration: 1.6,
-    ease: 'power4.out',
-    paused: true,
+  $('[hero-card]').on('mouseover', function () {
+    console.log('open');
+    this.animation.play();
   });
 
-  const cardItem = document.querySelector('.hero-item-content_wrapper');
-
-  cardItem.addEventListener('mouseover', () => {
-    animationTitles.play();
-    animationCard.play();
-  });
-
-  cardItem.addEventListener('mouseout', () => {
-    animationTitles.reverse();
-    animationCard.reverse();
+  $('[hero-card]').on('mouseout', function () {
+    console.log('close');
+    this.animation.reverse();
   });
 });
