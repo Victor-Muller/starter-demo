544a15d371cfa5d0810ef9da7698a4054a397746
Victor-Muller <victor.muller@abelia.agency>
2022-11-09 20:08:48 +0100

    optimisation animations

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,36 @@ import { gsap } from 'gsap';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  // Animation Hero Cards
+  // HERO CARDS ANIMATION
   $('[hero-card]').each(function () {
     let bigTitle = $(this).find('.hero-item-title_wrapper');
     let detailsTitle = $(this).find('.hero-details_h2');
     let detailsDivider = $(this).find('.hero-details_divider');
     let detailsText = $(this).find('.hero-details_text');
+    let button = $(this).find('.button');
     let card = $(this);
-    let tl = gsap.timeline();
+    let tlHero = gsap.timeline();
 
-    tl.to(card, { flexGrow: 4 }, 0)
-      .to(bigTitle, { scale: 1.4, ease: 'circ.out ' }, 0)
-      .from(detailsTitle, { opacity: 0 }, 0)
-      .from(detailsDivider, { width: 0 }, 0)
-      .from(detailsText, { opacity: 0, y: 20 }, 0)
+    tlHero
+      .to(card, { flexGrow: 3, ease: 'sine.out', duration: 0.5 }, 0)
+      .to(bigTitle, { scale: 1.4, ease: 'power4.out', duration: 0.5 }, 0)
+      .from(detailsTitle, { opacity: 0, duration: 0.5 }, 0)
+      .from(detailsDivider, { width: 0, duration: 0.5 }, 0)
+      .from(detailsText, { opacity: 0, y: 20, duration: 0.5 }, 0)
+      .from(button, { opacity: 0, y: 20, duration: 0.5 }, 0)
       .reversed(true);
-    this.animation = tl;
+    this.animation = tlHero;
   });
 
   $('[hero-card]').on('mouseover', function () {
-    console.log('open');
     this.animation.play();
   });
 
   $('[hero-card]').on('mouseout', function () {
-    console.log('close');
     this.animation.reverse();
   });
 
-  // Slick carousel
+  // FEATURES CAROUSEL
 
   $('.services-carousel-top_wrapper').slick({
     centerMode: true,
@@ -51,5 +52,19 @@ window.Webflow.push(() => {
     adaptiveHeight: true,
     fade: true,
     arrows: false,
+    speed: 0,
   });
+
+  // FEATURES CAROUSEL ANIMATION
+  $('.services-carousel-content_wrapper').on(
+    'afterChange',
+    function (event, slick, currentSlide, nextSlide) {
+      gsap.from('.services-carousel-item_content', {
+        opacity: 0,
+        y: 50,
+        ease: 'power4.out',
+        duration: 1,
+      });
+    }
+  );
 });
